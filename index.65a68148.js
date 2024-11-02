b79e11b934dff977dd39afde7b1a00069651b5f6
Pasha Shchukin <pasha4320692@gmail.com>
2024-11-02 19:46:31 +0200

    make build

---
+++ index.65a68148.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){var n=t.target.closest("a");n&&(t.preventDefault(),e.src=n.href)});
+//# sourceMappingURL=index.65a68148.js.map
